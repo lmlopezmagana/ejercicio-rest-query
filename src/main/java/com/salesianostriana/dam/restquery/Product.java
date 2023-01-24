b7c9e684e8d7a8993ddef8d2da87b707a1415d05
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2023-01-24 13:07:45 +0100

    Version changed

--- src/main/java/com/salesianostriana/dam/restquery/Product.java
+++ src/main/java/com/salesianostriana/dam/restquery/Product.java
@@ -1,14 +1,15 @@
 package com.salesianostriana.dam.restquery;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
